4484d2d188563f94bf43470e2cf9d1b97613e1db
allydunham <ally@ebi.ac.uk>
2019-10-14 10:49:41 +0100

    Added template for yaml

--- bin/setup_study_folder.sh
+++ bin/setup_study_folder.sh
@@ -3,7 +3,7 @@
 
 # Setup dir with yaml meta file and raw subdir
 mkdir "$1"
-touch "$1/$1.yaml"
 mkdir "$1/raw"
 
+cp "../../meta/study_template.yaml" "$1/$1.yaml"
 cp "../../bin/standardise_study_template.R" "$1/standardise_$1.R" || echo "Couldn't copy template R script"
