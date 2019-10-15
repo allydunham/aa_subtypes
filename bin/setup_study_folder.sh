82730fcd663abb9b66cc0e54b266cad09815cdd8
allydunham <ally@ebi.ac.uk>
2019-10-15 10:10:01 +0100

    Added stable locations for templates (now won't work on farm, but unlikely to use there)

--- bin/setup_study_folder.sh
+++ bin/setup_study_folder.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 # Setup a folder for a study with the given ID
 
-# Setup dir with yaml meta file and raw subdir
-mkdir "$1"
-mkdir "$1/raw"
+# Config
+proj_root="$HOME/phd/subtypes"
+template_yaml="$proj_root/meta/study_template.yaml"
+template_script="$proj_root/bin/standardise_study_template.R"
 
-cp "../../meta/study_template.yaml" "$1/$1.yaml" || echo "Couldn't copy template YAML"
-cp "../../bin/standardise_study_template.R" "$1/standardise_$1.R" || echo "Couldn't copy template R script"
+# Setup dir with yaml meta file and raw subdir
+mkdir "$1" || exit
+mkdir "$1/raw"
+cp "$template_yaml" "$1/$1.yaml" || echo "Couldn't copy template YAML"
+cp "$template_script" "$1/standardise_$1.R" || echo "Couldn't copy template R script"
