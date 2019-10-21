e67b628f798fe0c2453ecb1eb5aec2e07c91063e
allydunham <ally@ebi.ac.uk>
2019-10-21 15:19:29 +0100

    Added Weile et al. 2017 genes (CALM1, SUMO1, TPK1, UBE2I)

---
+++ data/studies/weile_2017_sumo1/standardise_weile_2017_sumo1.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+# Standardise data from Weile et al. 2017 (SUMO1)
+source('src/config.R')
+source('src/study_standardising.R')
+
+aa_code <- structure(names(Biostrings::AMINO_ACID_CODE), names=Biostrings::AMINO_ACID_CODE)
+
+# Import and process data
+meta <- read_yaml('data/studies/weile_2017_sumo1/weile_2017_sumo1.yaml')
+dm_data <- read_csv('data/studies/weile_2017_sumo1/raw/weile_2017_sumo1_score_comp.csv', na = c('NA','','None')) %>%
+  mutate(mut = str_replace_all(hgvs_pro, aa_code)) %>%
+  tidyr::extract(mut, into = c('wt', 'position', 'mut'), "p\\.([A-Z]+)([0-9]*)([A-Z=]+)", convert=TRUE) %>%
+  mutate(mut = if_else(mut == '=', wt, mut),
+         raw_score = score,
+         score = transform_vamp_seq(raw_score),
+         class = get_variant_class(wt, mut)) %>%
+  select(position, wt, mut, score, raw_score, class) %>%
+  arrange(position, mut)
+
+# Save output
+standardise_study(dm_data, meta$study, meta$transform)
---
+++ data/studies/weile_2017_ube2i/standardise_weile_2017_ube2i.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+# Standardise data from Weile et al. 2017 (UBE2I)
+source('src/config.R')
+source('src/study_standardising.R')
+
+aa_code <- structure(names(Biostrings::AMINO_ACID_CODE), names=Biostrings::AMINO_ACID_CODE)
+
+# Import and process data
+meta <- read_yaml('data/studies/weile_2017_ube2i/weile_2017_ube2i.yaml')
+dm_data <- read_csv('data/studies/weile_2017_ube2i/raw/weile_2017_ube2i_score_comp.csv', na = c('NA','','None')) %>%
+  mutate(mut = str_replace_all(hgvs_pro, aa_code)) %>%
+  tidyr::extract(mut, into = c('wt', 'position', 'mut'), "p\\.([A-Z]+)([0-9]*)([A-Z=]+)", convert=TRUE) %>%
+  mutate(mut = if_else(mut == '=', wt, mut),
+         raw_score = score,
+         score = transform_vamp_seq(raw_score),
+         class = get_variant_class(wt, mut)) %>%
+  select(position, wt, mut, score, raw_score, class) %>%
+  arrange(position, mut)
+
+# Save output
+standardise_study(dm_data, meta$study, meta$transform)
