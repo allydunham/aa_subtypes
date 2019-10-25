c694f6f05fb636ef195954c31000d8bc4bb94ebe
allydunham <ally@ebi.ac.uk>
2019-10-25 16:58:51 +0100

    Added class column

--- data/studies/hietpas_2011_hsp90/standardise_hietpas_2011_hsp90.R
+++ data/studies/hietpas_2011_hsp90/standardise_hietpas_2011_hsp90.R
@@ -1,6 +1,5 @@
 #!/usr/bin/env Rscript
 # Standardise data from Hietpas et al. 2011 (HSP90) 
-
 source('src/config.R')
 source('src/study_standardising.R')
 
@@ -9,9 +8,10 @@ meta <- read_yaml('data/studies/hietpas_2011_hsp90/hietpas_2011_hsp90.yaml')
 dm_data <- read_csv('data/studies/hietpas_2011_hsp90/raw/hietpas_2011_pdz_ligands_fitness.csv') %>%
   rename(mut = aa, raw_score = selection_coefficient) %>%
   mutate(wt = str_split(meta$seq, '')[[1]][position]) %>%
-  group_by(position, wt, mut) %>%
+  group_by(position, wt, mut) %>% # Average over codons
   summarise(raw_score = mean(raw_score)) %>%
-  mutate(score = normalise_score(raw_score))
+  mutate(score = normalise_score(raw_score),
+         class = get_variant_class(wt, mut))
 
 # Save output
 standardise_study(dm_data, meta$study, meta$transform)
