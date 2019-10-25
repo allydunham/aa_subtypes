b569eafd48e1855e593f72456c286c9b756afe2e
allydunham <ally@ebi.ac.uk>
2019-10-25 16:57:29 +0100

    Fixed bad yaml import

--- data/studies/ashenberg_2017_np/standardise_ashenberg_2017_np.R
+++ data/studies/ashenberg_2017_np/standardise_ashenberg_2017_np.R
@@ -1,11 +1,10 @@
 #!/usr/bin/env Rscript
 # Standardise data from Ashenberg et al. 2017 (Flu NP)
-
 source('src/config.R')
 source('src/study_standardising.R')
 
 # Import and process data
-meta <- read_csv('data/studies/ashenberg_2017_np/ashenberg_2017_np.yaml')
+meta <- read_yaml('data/studies/ashenberg_2017_np/ashenberg_2017_np.yaml')
 dm_data <- read_csv('data/studies/ashenberg_2017_np/raw/ashenberg_2017_flu_np.csv') %>%
   rename(position = site, raw_score = diffsel) %>%
   mutate(score = normalise_score(raw_score),
