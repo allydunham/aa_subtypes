c29986d931f65f11c467489ca90fb64f9a6d05d8
allydunham <ally@ebi.ac.uk>
2019-10-25 16:58:00 +0100

    Fixed missing position column

--- data/studies/brenan_2016_mapk1/standardise_brenan_2016_mapk1.R
+++ data/studies/brenan_2016_mapk1/standardise_brenan_2016_mapk1.R
@@ -7,12 +7,12 @@ source('src/study_standardising.R')
 meta <- read_yaml('data/studies/brenan_2016_mapk1/brenan_2016_mapk1.yaml')
 dm_data <- read_xlsx('data/studies/brenan_2016_mapk1/raw/brenan_2016_erk2.xlsx', sheet = 'Supplemental_Table_1') %>%
   rename_all(list( ~ gsub(' ', '_', tolower(.)))) %>%
-  rename(wt = wt_aa, mut = mutant_aa) %>%
-  mutate(raw_score = `lfc_(etp_vs._dox)`,
-         score = -raw_score, # The selection scheme they used favoured lof > wt > gof
-         score = normalise_score(raw_score),
+  rename(wt = wt_aa, mut = mutant_aa, position = erk2_residue) %>%
+  mutate(raw_score = `lfc_(etp_vs._dox)`, # Only general condition, other two are for specific drugs
+         score = normalise_score(-raw_score), # The selection scheme they used favoured lof > wt > gof
          class = get_variant_class(wt, mut)) %>% 
-  mutate_at(vars(nuc_acid_changes, dox_rank, sch_rank, vrt_rank, vrt_specific_allele, sch_specific_allele), as.integer)
+  mutate_at(vars(nuc_acid_changes, dox_rank, sch_rank, vrt_rank, vrt_specific_allele, sch_specific_allele), as.integer) %>%
+  select(position, wt, mut, score, raw_score, class)
 
 # Save output
 standardise_study(dm_data, meta$study, meta$transform)
