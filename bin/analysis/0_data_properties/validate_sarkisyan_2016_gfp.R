144b16c53ee6b092127aaf79669a8043244f43c8
allydunham <ally@ebi.ac.uk>
2019-10-29 10:37:25 +0000

    Added fraction of variants with single variant based scores to subtitle

--- bin/analysis/0_data_properties/validate_sarkisyan_2016_gfp.R
+++ bin/analysis/0_data_properties/validate_sarkisyan_2016_gfp.R
@@ -21,8 +21,8 @@ dm_data <- separate(raw_data, mut, into = str_c('mut', 1:15), sep = ':', fill = 
     group_by(position, wt, mut)
 
 singles <- summarise(dm_data, single_score := ifelse(any(n_mut == 1), mean(median_brightness[n_mut == 1], na.rm = TRUE), NA)) %>%
-  ungroup() %>%
-  mutate(frac = sum(!is.na(single_score))/length(single_score))
+  ungroup()
+single_frac <- sum(!is.na(singles$single_score))/length(singles$single_score)
 
 mut_count_data <- lapply(2:15, function(x){
   summarise(dm_data, !!str_c('score_', x) := ifelse(any(n_mut <= x), mean(median_brightness[n_mut <= x], na.rm = TRUE), NA)) %>%
@@ -33,7 +33,8 @@ mut_count_data <- lapply(2:15, function(x){
   pivot_longer(starts_with('score_'), names_to = 'n_mut', values_to = 'multi_score', names_prefix = 'score_', names_ptypes = list(n_mut=integer())) %>%
   group_by(n_mut) %>%
   mutate(frac = sum(!is.na(multi_score))/length(multi_score)) %>%
-  ungroup()
+  ungroup() %>%
+  mutate(n_mut = as.integer(n_mut))
 
 p_sing_multi <- ggplot(mut_count_data, aes(x = single_score, y = multi_score)) +
   facet_wrap(~n_mut, ncol = 5) +
@@ -42,5 +43,6 @@ p_sing_multi <- ggplot(mut_count_data, aes(x = single_score, y = multi_score)) +
   geom_text(x = 1.5, y = 3.5, aes(label = str_c('frac = ', signif(frac, digits = 4))), hjust = 0) +
   labs(x = 'Median Brightness (Single Variant)',
        y = 'Median Brightness (Mean Over Multiple Variants)',
-       title = 'Accuracy of multi-variant averaging for scoring in Sarkisyan et al. 2016 (GFP)')
+       title = 'Accuracy of multi-variant averaging for scoring in Sarkisyan et al. 2016 (GFP)',
+       subtitle = str_c('Fraction of variants with individual measures: ', single_frac))
 ggsave('figures/0_data_properties/sarkisyan_2016_gfp/multi_mut_validation.pdf', p_sing_multi, units = 'cm', height = 15, width = 25)
