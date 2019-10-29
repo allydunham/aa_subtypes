49a5bdc7f6e16c3d77dfc7bb648036f3f0edb838
allydunham <ally@ebi.ac.uk>
2019-10-29 11:41:21 +0000

    Added hartman_2018_cp

---
+++ data/studies/hartman_2018_cp/standardise_hartman_2018_cp.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+# Standardise data from Hartman et al. 2018 (CP)
+source('src/config.R')
+source('src/study_standardising.R')
+
+# Import and process data
+meta <- read_yaml('data/studies/hartman_2018_cp/hartman_2018_cp.yaml')
+dm_data <- read_xlsx('data/studies/hartman_2018_cp/raw/41467_2018_3783_MOESM4_ESM.xlsx', skip = 1, na = 'Not Observed') %>%
+  rename(position = `Residue #`) %>%
+  pivot_longer(-position, names_to = 'mut', values_to = 'raw_score') %>%
+  mutate(score = normalise_score(raw_score),
+         position = position + 1,
+         wt = str_split(meta$seq, '')[[1]][position],
+         class = get_variant_class(wt, mut))
+
+# Save output
+standardise_study(dm_data, meta$study, meta$transform)
