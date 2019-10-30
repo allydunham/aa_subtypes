bece436db7e04f4f970af008bbe9cc90ca61c736
allydunham <ally@ebi.ac.uk>
2019-10-30 17:20:33 +0000

    Added kelsic_2016_infa

---
+++ data/studies/kelsic_2016_infa/standardise_kelsic_2016_infa.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+# Standardise data from Kelsic et al. 2016 (infA)
+
+source('src/config.R')
+source('src/study_standardising.R')
+
+# Import and process data
+meta <- read_yaml('data/studies/kelsic_2016_infa/kelsic_2016_infa.yaml')
+dm_data <- read_csv('data/studies/kelsic_2016_infa/raw/cels_206_mmc5.csv', skip = 1,
+                    col_names = c('codon', 'mut', 'position', 'is_wt', 'raw_score',
+                                  'sd', 'fitness_rich',
+                                  'fitness_stdev_rich', 'RCS',
+                                  'mfe_ddG_43nt_sliding', 'tmp')) %>%
+  select(codon:sd) %>%
+  mutate(score = transform_vamp_seq(raw_score, normalise = FALSE)) %>%
+  group_by(position, mut) %>%
+  summarise_at(vars(score, raw_score), mean, na.rm=TRUE) %>%
+  ungroup() %>%
+  mutate(score = normalise_score(score),
+         wt = str_split(meta$seq, '')[[1]][position],
+         class = get_variant_class(wt, mut))
+
+# Save output
+standardise_study(dm_data, meta$study, meta$transform)
