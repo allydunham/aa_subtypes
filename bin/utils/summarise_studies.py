c387fe0c4ab213357a4746556623d504b9828aeb
allydunham <ally@ebi.ac.uk>
2019-11-04 15:02:30 +0000

    Added Coverage stats plus gene and overall summary

--- bin/utils/summarise_studies.py
+++ bin/utils/summarise_studies.py
@@ -13,6 +13,7 @@ def main(args):
     yaml = YAML(typ='safe')
 
     study_data = defaultdict(list)
+    gene_data = {}
     for study_dir in args.dirs:
         study_dir = study_dir.rstrip('/')
         study = study_dir.split('/')[-1]
@@ -20,26 +21,90 @@ def main(args):
         with open(f'{study_dir}/{study}.yaml', 'r') as yaml_file:
             meta = yaml.load(yaml_file)
 
+        gene = meta['gene']
+
+        # calculate study data
         variants = pd.read_csv(f'{study_dir}/{study}.tsv', sep='\t')
-        positions = 0
-        coverage = 0
-        completeness = 0
+        positions = variants.position.nunique()
+        total = variants[~(variants.mut == '*')].shape[0]
+        coverage = round(variants[variants['class'] == 'Missense'].shape[0]/(positions*19), 3)
+        nonsense = 'Nonsense' in variants['class'].values
+        synonymous = 'Synonymous' in variants['class'].values
 
         study_data['Study'].append(meta['study'])
         study_data['Authour'].append(meta['authour'])
         study_data['Year'].append(meta['year'])
-        study_data['Gene'].append(meta['gene'])
+        study_data['Gene'].append(gene)
         study_data['Function'].append(meta['gene_type'])
         study_data['Species'].append(meta['species'])
         study_data['Experiment'].append(meta['experiment'])
         study_data['Transform'].append(meta['transform'])
         study_data['Filtered'].append(meta['qc']['filter'])
-        study_data['Positions'].append(positions)
-        study_data['Coverage'].append(coverage)
-        study_data['Completeness'].append(completeness)
+        study_data['Gene Length'].append(len(meta['seq']))
+        study_data['Mutated Positions'].append(positions)
+        study_data['Variants (Missense)'].append(total)
+        study_data['Coverage (Missense)'].append(coverage)
+        study_data['Nonsense'].append(nonsense)
+        study_data['Synonymous'].append(synonymous)
 
+        # Add to gene data
+        if not meta['qc']['filter'] and gene in gene_data:
+            gene_data[gene]['df'] = pd.merge(gene_data[gene]['df'],
+                                             variants[['position', 'wt', 'mut', 'class']],
+                                             how='outer', on=['position', 'wt', 'mut', 'class'])
+
+            gene_data[gene]['studies'] += 1
+
+            if not gene_data[gene]['length'] == len(meta['seq']):
+                raise ValueError(f'{gene}: Lengths do not match')
+
+            if not gene_data[gene]['function'] == meta['gene_type']:
+                raise ValueError(f'{gene}: Functions do not match')
+
+            if not gene_data[gene]['species'] == meta['species']:
+                raise ValueError(f'{gene}: Species do not match')
+
+        elif not meta['qc']['filter']:
+            gene_data[gene] = {'df': variants[['position', 'wt', 'mut', 'class']],
+                               'studies': 1, 'length': len(meta['seq']),
+                               'function': meta['gene_type'],
+                               'species': meta['species']}
+
+    # Write study table
     study_data = pd.DataFrame(study_data)
     study_data.to_csv(args.studies, sep='\t', index=False)
+
+    # Calculate gene table
+    gene_df = defaultdict(list)
+    for gene, data in gene_data.items():
+        positions = data['df'].position.nunique()
+        variants = data['df'][~(data['df'].mut == '*')].shape[0]
+        coverage = round(data['df'][data['df']['class'] == 'Missense'].shape[0]/(positions*19), 3)
+
+        gene_df['Gene'].append(gene)
+        gene_df['Function'].append(data['function'])
+        gene_df['Species'].append(data['species'])
+        gene_df['Studies'].append(data['studies'])
+        gene_df['Length'].append(data['length'])
+        gene_df['Mutated Positions'].append(positions)
+        gene_df['Variants (Missense)'].append(variants)
+        gene_df['Coverage (Missense)'].append(coverage)
+
+    gene_df = pd.DataFrame(gene_df)
+    gene_df.to_csv(args.genes, sep='\t', index=False)
+
+    # Calculate Summary
+    with open(args.summary, 'w') as summary_file:
+        print('Summary statistics for Subtypes project', file=summary_file)
+        print('Total Studies:', study_data.shape[0], file=summary_file)
+        print('Unfiltered Studies:', sum(~study_data['Filtered']), file=summary_file)
+        print('Genes:', gene_df.shape[0], file=summary_file)
+        print('Positions:', sum(gene_df['Mutated Positions']), file=summary_file)
+        print('Missense Variants:', sum(gene_df['Variants (Missense)']), file=summary_file)
+        print('Average Coverage:',
+              sum(gene_df['Coverage (Missense)'])/gene_df.shape[0],
+              file=summary_file)
+
 
 def parse_args():
     """Process input arguments"""
@@ -51,7 +116,7 @@ def parse_args():
     parser.add_argument('--studies', '-s', help="Output file for study table",
                         default='studies.tsv')
 
-    parser.add_argument('--genes', '-g', help="Output file for genes table [NOT IMPLEMENTED]",
+    parser.add_argument('--genes', '-g', help="Output file for genes table",
                         default='genes.tsv')
 
     parser.add_argument('--summary', '-u', help="Output file for summary table",
