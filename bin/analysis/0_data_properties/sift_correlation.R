f4094df218f8c983bf61455067701d051a257d66
allydunham <ally@ebi.ac.uk>
2019-11-06 11:40:19 +0000

    Added colour for filtered studies in plot

--- bin/analysis/0_data_properties/sift_correlation.R
+++ bin/analysis/0_data_properties/sift_correlation.R
@@ -47,6 +47,12 @@ sift_correlations <- bind_rows(group_by(dms, study) %>%
                                  do(tidy(cor.test(.$score, .$log10_sift, method = 'pearson')))) %>%
   mutate(study_pretty = sapply(study, format_study, USE.NAMES = FALSE),
          p_cat = pretty_p_values(p.value, breaks = c(1e-48, 1e-12, 1e-06, 1e-3, 0.01, 0.05)))
+
+filtered <- sapply(unique(sift_correlations$study), function(x){
+  y <- read_yaml(str_c('data/studies/', x, '/', x, '.yaml'))
+  return(ifelse(y$qc$filter, 'red', 'black'))
+})
+names(filtered) <- sapply(names(filtered), format_study, USE.NAMES = FALSE)
   
 p_sift_cor <- ggplot(sift_correlations, aes(x = study_pretty, y = estimate, fill = p_cat)) +
   facet_wrap(~method, ncol = 1) +
@@ -57,5 +63,6 @@ p_sift_cor <- ggplot(sift_correlations, aes(x = study_pretty, y = estimate, fill
   xlab('') +
   ylab('Correlation') +
   scale_fill_viridis_d(guide=guide_legend(title='p-value'), drop=FALSE) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5,
+                                   colour = filtered[unique(sift_correlations$study_pretty)]))
 ggsave('figures/0_data_properties/sift_score_correlation.pdf', p_sift_cor, width = 20, height = 20, units = 'cm')
