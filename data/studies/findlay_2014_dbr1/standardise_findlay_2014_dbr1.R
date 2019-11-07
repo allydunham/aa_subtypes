be53a6264e9982d092fec8011da3488b97c24f79
allydunham <ally@ebi.ac.uk>
2019-11-07 15:57:55 +0000

    Added codon averaging

--- data/studies/findlay_2014_dbr1/standardise_findlay_2014_dbr1.R
+++ data/studies/findlay_2014_dbr1/standardise_findlay_2014_dbr1.R
@@ -5,18 +5,16 @@ source('src/study_standardising.R')
 
 # Import and process data
 meta <- read_yaml('data/studies/findlay_2014_dbr1/findlay_2014_dbr1.yaml')
-dm_data <- read_xlsx('data/studies/findlay_2014_dbr1/raw/findlay_2014_dbr1_exon2_counts.xlsx', skip = 3, na = 'NA') %>%
-  rename(seq = Sequence,
-         log2_enrichment_score_day11_rep1 = `Day 11 log2 enrichment score (replicate 1)`,
-         log2_enrichment_score_day11_rep2 = `Day 11 log2 enrichment score (replicate 2)`,
-         position = `Affected codon`,
-         wt = `Reference AA`,
-         mut = `Substituted AA`,
-         mut_type = `Variant Class`) %>%
-  mutate(position = as.integer(position),
+dm_data <- read_xlsx('data/studies/findlay_2014_dbr1/raw/findlay_2014_dbr1_exon2_counts.xlsx', skip = 5, na = 'NA',
+                     col_names = c('seq', 'log2_enrichment_score_day11_rep1', 'log2_enrichment_score_day11_rep2',
+                                   'position', 'wt', 'mut', 'mut_type')) %>%
+  mutate(position = as.integer(na_if(position, "3'SS")),
          mut = ifelse(mut == 'WT', wt, mut),
-         raw_score = rowMeans(select(., log2_enrichment_score_day11_rep1, log2_enrichment_score_day11_rep2), na.rm = TRUE) %>% replace_na(NA),
-         transformed_score = raw_score,
+         raw_score = rowMeans(select(., log2_enrichment_score_day11_rep1, log2_enrichment_score_day11_rep2), na.rm = TRUE) %>% replace_na(NA)) %>%
+  group_by(position, wt, mut) %>%
+  summarise(raw_score = mean(raw_score, na.rm=TRUE)) %>%
+  ungroup() %>%
+  mutate(transformed_score = raw_score,
          score = normalise_score(transformed_score),
          class = get_variant_class(wt, mut)) %>%
   drop_na(position, raw_score) %>%
