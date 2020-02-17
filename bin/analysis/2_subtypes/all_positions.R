9c6ead0cf5f05d4a828f75d8bb63f8695d839d68
allydunham <ally@ebi.ac.uk>
2020-02-17 17:27:05 +0000

    Updated to new cosine based dynamic hclust, plus started working on revamping plots

--- bin/analysis/2_subtypes/all_positions.R
+++ bin/analysis/2_subtypes/all_positions.R
@@ -1,18 +1,26 @@
 #!/usr/bin/env Rscript
 # Perform clustering on all positions at once
 source('src/config.R')
-source('src/subtypes.R')
+source('src/subtype_characterisation.R')
 
-dms_wide <- read_tsv('data/combined_mutational_scans.tsv')
+dms <- read_tsv('data/combined_mutational_scans.tsv')
 
 ### Make Dynamic Hclust Clusters ###
-clusters <- make_dynamic_hclust_clusters(dms_wide, PC2:PC10, dist_method = 'manhattan', treecut_args = list(deepSplit=2))
-dms_wide <- mutate(clusters$tbl, cluster = str_c('X', cluster))
+clusters <- make_dynamic_hclust_clusters(dms, PC2:PC20, distance_method = 'cosine', treecut_args = list(deepSplit=0))
+dms <- mutate(clusters$tbl, cluster = factor(str_c('X', cluster), levels = str_c('X', c(1:(n_distinct(cluster) - 1), 0)))) %>% # -1 as includes 0
+  select(cluster, everything())
 
 ### Analyse clusters ###
-plots <- make_cluster_plots(dms_wide, cols = A:Y, chem_env_cols = within_10_0_A:within_10_0_Y, clusters = list(X=clusters))
+n_clusters <- n_distinct(dms$cluster)
 
-cluster_occupancy <- group_by(dms_wide, cluster, wt) %>%
+plots <- list()
+plots$umap <- labeled_plot(plot_cluster_umap(dms), units = 'cm', height = 20, width = 20)
+plots$tsne <- labeled_plot(plot_cluster_tsne(dms), units = 'cm', height = 20, width = 20)
+plots$silhouette_global <- labeled_plot(plot_silhouette(dms, A:Y), units='cm', height = n_clusters*0.33 + 2, width = 15, limitsize=FALSE)
+plots$silhouette_per_aa <- labeled_plot(plot_per_aa_silhouette(dms, A:Y), units='cm', height = n_clusters*0.33 + 2, width = 15, limitsize=FALSE)
+plots$silhouette_per_aa_cosine <- labeled_plot(plot_per_aa_silhouette(dms, A:Y, 'cosine'), units='cm', height = n_clusters*0.33 + 2, width = 15, limitsize=FALSE)
+
+cluster_occupancy <- group_by(dms, cluster, wt) %>%
   tally() %>%
   mutate(rel_n = n / sum(n))
 
@@ -27,7 +35,7 @@ plots$cluster_occupancy <- filter(cluster_occupancy, !str_ends(cluster, '0')) %>
         panel.grid.major.y = element_blank())
 
 ### Save results ###
-write_tsv(select(dms_wide, cluster, study, gene, position, wt), 'data/clustering/a.tsv')
+write_tsv(select(dms, cluster, study, gene, position, wt), 'data/clustering/a.tsv')
 root <- 'figures/2_clustering/hclust_profile_dynamic_all_positions'
 dir.create(root)
 save_plotlist(plots, root)
