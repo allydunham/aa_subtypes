3cdaafb3b7af739affcd0d2d339621119189ba3b
allydunham <ally@ebi.ac.uk>
2020-02-26 16:59:11 +0000

    Added sections loading for easy import of data for checking positions

--- bin/analysis/2_subtypes/final_subtypes.R
+++ bin/analysis/2_subtypes/final_subtypes.R
@@ -4,10 +4,13 @@ source('src/config.R')
 source('src/subtype_characterisation.R')
 plots <- list()
 
+sections <- 
 dms <- left_join(rename(read_tsv('data/subtypes/hclust_pca_no_sig_dynamic_cos_deep_0.tsv'), cluster_ds0 = cluster),
                  rename(read_tsv('data/subtypes/hclust_pca_no_sig_dynamic_cos_deep_1.tsv'), cluster_ds1 = cluster),
                  by = c("study", "gene", "position", "wt")) %>%
   left_join(read_tsv('data/combined_mutational_scans.tsv'), by = c("study", "gene", "position", "wt")) %>%
   select(cluster_ds0, cluster_ds1, everything())
+
+pdb_pos <- dms_pdb_positions(dms)
 
 full_characterisation <- full_cluster_characterisation(select(dms, cluster = cluster_ds0, everything()))
