cabaec36b3c9435f8dfb21406522410629ad25b0
allydunham <ally@ebi.ac.uk>
2020-03-09 14:31:38 +0000

    Switched final subtype used to no_permissive set

--- bin/analysis/2_subtypes/final_subtypes.R
+++ bin/analysis/2_subtypes/final_subtypes.R
@@ -6,8 +6,8 @@ plots <- list()
 
 ### Import Data ###
 sections <- map(read_yaml('meta/structures.yaml'), extract2, 'sections')
-dms <- left_join(rename(read_tsv('data/subtypes/hclust_pca_no_sig_dynamic_cos_deep_0.tsv'), cluster_ds0 = cluster),
-                 rename(read_tsv('data/subtypes/hclust_pca_no_sig_dynamic_cos_deep_1.tsv'), cluster_ds1 = cluster),
+dms <- left_join(rename(read_tsv('data/subtypes/hclust_pca_no_sig_dynamic_cos_deep_0_no_permissive.tsv'), cluster_ds0 = cluster),
+                 rename(read_tsv('data/subtypes/hclust_pca_no_sig_dynamic_cos_deep_1_no_permissive.tsv'), cluster_ds1 = cluster),
                  by = c("study", "gene", "position", "wt")) %>%
   left_join(read_tsv('data/combined_mutational_scans.tsv'), by = c("study", "gene", "position", "wt")) %>%
   select(cluster_ds0, cluster_ds1, everything())
