46d5fc51d9310d0842c1c12d6d63558b3b8ca6ff
allydunham <ally@ebi.ac.uk>
2020-04-09 18:20:31 +0100

    Created better layout and made some panel clearer

--- bin/figures/figure4.R
+++ bin/figures/figure4.R
@@ -15,8 +15,8 @@ er_limits <- c(min(full_characterisation$profiles$er), -min(full_characterisatio
 
 plot_profiles <- function(clusters, legend = FALSE){
   filter(full_characterisation$profiles, cluster %in% clusters) %>%
-    mutate(cluster = add_markdown(cluster, cluster_colourmap(cluster)),
-           mut = add_markdown(mut, AA_COLOURS)) %>%
+    mutate(mut = add_markdown(mut, AA_COLOURS),
+           cluster = add_markdown(cluster, cluster_number_colourmap(cluster))) %>%
     ggplot(aes(x = mut, y = cluster, fill = er)) +
     geom_raster(show.legend = legend) +
     coord_fixed() +
@@ -42,59 +42,55 @@ p_cys_surface_acc <- filter(dms, cluster %in% c('C1', 'C2')) %>%
 p_cys_disulphide <- filter(dms, cluster %in% c('C1', 'C2'), !is.na(disulfide)) %>%
   ggplot(aes(x = cluster, fill = cluster)) +
   geom_bar(show.legend = FALSE) +
+  labs(y = 'Disulphide Bonds', x = '') +
   coord_flip() +
   scale_fill_brewer(type = 'qual', palette = 'Set1') +
   theme(legend.title = element_blank(),
-        axis.title = element_blank(),
         panel.grid.major.y = element_blank())
 
-p_cys_disulphide_ex <- ggplot() +
-  geom_blank() +
-  annotation_custom(readPNG('figures/4_figures/position_examples/ccr5_cys_disulphide.png') %>% rasterGrob(interpolate = TRUE),
-                    xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
-
-p_cys_2_aromatic_ex <- ggplot() +
-  geom_blank() +
-  annotation_custom(readPNG('figures/4_figures/position_examples/ccr5_cys_aromatic.png') %>% rasterGrob(interpolate = TRUE),
-                    xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
-
-p_cys_1_aromatic_ex <- ggplot() +
-  geom_blank() +
-  annotation_custom(readPNG('figures/4_figures/position_examples/np_cys_aromatic.png') %>% rasterGrob(interpolate = TRUE),
-                    xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
-
-p_cys_zinc_ex <- ggplot() +
-  geom_blank() +
-  annotation_custom(readPNG('figures/4_figures/position_examples/gal4_cys_zinc.png') %>% rasterGrob(interpolate = TRUE),
-                    xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
-
-p_cys <- blank_plot('C examples')
-
 ### Example 2 - D/E examples ###
-p_charge_profiles <- plot_profiles(c('D1', 'D2', 'D3', 'E1', 'E2', 'E3'))
+charge_groups <- c(D1='Negative', D2='Polar', D3='Not Proline', E1='Negative', E2='Not Proline', E3='Polar',
+                   K1='Positive', K2='Not Negative', K3='Not Proline', R1='Positive', R2='Not Proline', R3='Not Negative')
 
-p_charge_foldx <- filter(dms, cluster %in% c('D1', 'D2', 'D3', 'E1', 'E2', 'E3'), !is.na(electrostatics)) %>%
-  mutate(cluster_num = str_sub(cluster, -1)) %>%
-  ggplot(aes(x = cluster, y = electrostatics, fill = cluster_num)) +
-  geom_boxplot(show.legend = FALSE) +
-  scale_fill_brewer(type = 'qual', palette = 'Set1') +
+charge_profiles <- filter(full_characterisation$profiles, cluster %in% names(charge_groups)) %>%
+  mutate(group = charge_groups[cluster]) %>%
+  group_by(group, mut) %>%
+  summarise(er = mean(er)) %>%
+  ungroup() %>%
+  mutate(mut = add_markdown(mut, AA_COLOURS),
+         group = as.factor(group))
+
+group_labs <- levels(charge_profiles$group)
+subtype_labs <- c(Negative="<span style = 'color:#e41a1c'>D1</span>, <span style = 'color:#e41a1c'>E1</span>",
+                  Positive="<span style = 'color:#e41a1c'>K1</span>, <span style = 'color:#e41a1c'>R1</span>",
+                  Polar="<span style = 'color:#377eb8'>D2</span>, <span style = 'color:#4daf4a'>E3</span>",
+                  `Not Proline`="<span style = 'color:#4daf4a'>D3</span>, <span style = 'color:#377eb8'>E2</span>, <span style = 'color:#4daf4a'>K3</span>, <span style = 'color:#377eb8'>R2</span>",
+                  `Not Negative`="<span style = 'color:#377eb8'>K2</span>, <span style = 'color:#4daf4a'>R3</span>")[group_labs]
+
+p_charge_profiles <- ggplot(charge_profiles, aes(x = mut, y = as.integer(group), fill = er)) +
+  geom_raster(show.legend = FALSE) +
+  coord_fixed() +
+  scale_y_continuous(breaks = 1:length(group_labs), labels = group_labs,
+                     sec.axis = sec_axis(~., breaks = 1:length(subtype_labs), labels = subtype_labs)) +
+  scale_fill_distiller(type = ER_PROFILE_COLOURS$type, palette = ER_PROFILE_COLOURS$palette, direction = ER_PROFILE_COLOURS$direction, limits = er_limits) +
+  guides(fill = guide_colourbar(title = 'Normalised ER')) + 
+  theme(axis.text.x = element_markdown(),
+        axis.text.y.right = element_markdown(),
+        axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid.major.y = element_blank())
+
+p_charge_foldx <- filter(dms, cluster %in% names(charge_groups)) %>%
+  mutate(group = charge_groups[cluster]) %>%
+  ggplot(aes(x = group, y = electrostatics)) +
+  geom_boxplot(show.legend = FALSE, fill = '#377eb8', outlier.shape = 20) +
+  geom_hline(yintercept = 0, linetype = 'dotted', colour = 'black') +
   coord_flip() +
+  scale_fill_brewer(type = 'qual', palette = 'Set1') +
   guides(fill = guide_legend(title = 'Subtype')) +
-  labs(x = '', y = expression('Mean FoldX Electrostatic'~Delta*'G (kj mol'^-1*')')) +
+  labs(x = '', y = expression('Mean Substitution Electrostatic'~Delta*Delta*'G (kj mol'^-1*')')) +
   theme(panel.grid.major.y = element_blank(),
-        axis.ticks.y = element_blank())
-
-p_asp_ionic <- ggplot() +
-  geom_blank() +
-  annotation_custom(readPNG('figures/4_figures/position_examples/cbs_asp_ionic.png') %>% rasterGrob(interpolate = TRUE),
-                    xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
-
-p_asp_ligand <- ggplot() +
-  geom_blank() +
-  annotation_custom(readPNG('figures/4_figures/position_examples/tem1_asp_ligand.png') %>% rasterGrob(interpolate = TRUE),
-                    xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
-
-p_charge <- blank_plot('D/E (R/K) Examples')
+        axis.ticks = element_blank())
 
 ### Example 3 - G1/G3 examples ###
 p_gly_profiles <- plot_profiles(c('G1', 'G2', 'G3'))
@@ -130,44 +126,40 @@ p_tyr_profiles <- plot_profiles(c('Y1', 'Y2', 'Y3', 'Y4', 'YP'))
 
 p_tyr_surface_acc <- filter(dms, cluster %in% c('Y1', 'Y2', 'Y3', 'Y4', 'YP')) %>%
   ggplot(aes(x = all_atom_abs, y = ..scaled.., colour = cluster)) +
-  geom_line(stat = 'density') +
+  geom_line(stat = 'density', show.legend = FALSE) +
   scale_colour_brewer(type = 'qual', palette = 'Set1') +
   labs(x = 'Surface Accessibility (All Atom Abs)', y = 'Scaled Density') +
   theme(legend.title = element_blank())
 
 tyr_dssp <- filter(dms, cluster %in% c('Y1', 'Y2', 'Y3', 'Y4', 'YP')) %>%
-  select(cluster, ss_h, ss_e, ss_c) %>%
-  pivot_longer(-cluster, names_to = 'term', names_prefix = 'ss_', values_to = 'p') %>%
-  mutate(term = DSSP_CLASSES_PLOTMATH[term])
+  select(cluster, study, position, starts_with('ss_')) %>%
+  pivot_longer(starts_with('ss_'), names_to = 'term', names_prefix = 'ss_', values_to = 'p') %>%
+  group_by(cluster, study, position) %>%
+  summarise(ss = DSSP_CLASSES_PLOTMATH[term[which.max(p)]]) %>%
+  ungroup() %>%
+  count(cluster, ss) %>%
+  group_by(cluster) %>%
+  mutate(prop = n / sum(n))
 
-p_tyr_dssp <- ggplot(tyr_dssp, aes(x = cluster, y = p, fill = term)) +
-  geom_boxplot() +
-  scale_fill_brewer(labels = parse(text = sort(unique(tyr_dssp$term))), type = 'qual', palette = 'Dark2') +
-  labs(y = 'Probability', x = '') +
+p_tyr_dssp <- ggplot(tyr_dssp, aes(x = cluster, y = prop, fill = ss)) +
+  geom_col() +
+  scale_fill_brewer(labels = function(x){parse(text=x)}, type = 'qual', palette = 'Dark2') +
+  coord_flip() +
+  labs(y = 'Proportion', x = '') +
+  guides(fill = guide_legend(title = '', reverse = TRUE)) +
   theme(panel.grid.major.y = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank(),
+        legend.position = 'top')
 
 p_tyr_haem <- ggplot() +
   geom_blank() +
   annotation_custom(readPNG('figures/4_figures/position_examples/cbs_tyr_haem.png') %>% rasterGrob(interpolate = TRUE),
                     xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
 
-p_tyr_not_proline <- ggplot() +
-  geom_blank() +
-  annotation_custom(readPNG('figures/4_figures/position_examples/pab1_tyr_not_proline.png') %>% rasterGrob(interpolate = TRUE),
-                    xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
-
 p_tyr_pi <- ggplot() +
   geom_blank() +
   annotation_custom(readPNG('figures/4_figures/position_examples/tp53_tyr_pi.png') %>% rasterGrob(interpolate = TRUE),
                     xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
-
-p_tyr_buried <- ggplot() +
-  geom_blank() +
-  annotation_custom(readPNG('figures/4_figures/position_examples/aph3ii_tyr_buried.png') %>% rasterGrob(interpolate = TRUE),
-                    xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
-
-p_tyr <- blank_plot('Y Examples')
 
 ### Example 5 - Large/Small hydrophobics ###
 large_hydrophobics <- c('I2', 'L2', 'M1')
@@ -193,48 +185,107 @@ p_hydro_foldx <- filter(dms, cluster %in% c(large_hydrophobics, small_hydrophobi
   theme(panel.grid.major.y = element_blank(),
         axis.ticks.y = element_blank())
 
-p_hydro <- blank_plot('Large/Small/Aromatic Example')
-
 ### Assemble figure ###
 size <- theme(text = element_text(size = 8))
 er_legend <- plot_profiles('A1', legend = TRUE) %>% get_legend() %>% as_ggplot() + size
 
-p1_prof <- p_cys_profiles + size + labs(tag = 'A')
-p1_disulphide <- p_cys_disulphide + size
-p1_disulphide_ex <- p_cys_disulphide_ex + size
-p1_sa <- p_cys_surface_acc + size
-p1_zinc <- p_cys_zinc_ex + size
-p1_aromatic_1 <- p_cys_1_aromatic_ex + size
-p1_aromatic_2 <- p_cys_2_aromatic_ex + size
+# Panel 1
+p1 <- ggplot() +
+  geom_blank() +
+  lims(x = c(0, 1), y = c(0, 1)) +
+  coord_fixed() +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.grid.major.x = element_line(colour = 'grey', linetype = 'dotted')) +
+  size +
+  labs(tag = 'A') +
+  annotation_custom(ggplotGrob(p_cys_profiles + size), xmin = 0.15, xmax = 0.8, ymin = 0.4, ymax = 0.6) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/ccr5_cys_disulphide.png'), interpolate = TRUE,
+                    xmin = 0.025, xmax = 0.2, ymin = 0.7, ymax = 0.875) +
+  annotation_custom(ggplotGrob(p_cys_disulphide + size), xmin = 0.2, xmax = 0.475, ymin = 0.7, ymax = 0.875) +
+  annotate('text', x = 0.25, y = 0.65, label = str_wrap('Most disulphide bonds are C1 positions', 25)) + 
+  annotation_custom(ggplotGrob(p_cys_surface_acc + size), xmin = 0.525, xmax = 0.975, ymin = 0.7, ymax = 0.975) +
+  annotate('text', x = 0.75, y = 0.65, label = str_wrap('C2 positions are more buried', 25)) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/gal4_cys_zinc.png'), interpolate = TRUE,
+                    xmin = 0.1, xmax = 0.4, ymin = 0.1, ymax = 0.4) +
+  annotate('text', x = 0.25, y = 0.05, label = str_wrap('Other active roles are generally C1 positions', 25)) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/ccr5_cys_aromatic.png'), interpolate = TRUE,
+                    xmin = 0.525, xmax = 0.73, ymin = 0.15, ymax = 0.355) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/np_cys_aromatic.png'), interpolate = TRUE,
+                    xmin = 0.77, xmax = 0.975, ymin = 0.15, ymax = 0.355) +
+  annotate('text', x = 0.75, y = 0.05, label = str_wrap('Both C1 & C2 positions have aromatic interactions', 30))
 
-p2 <- p_charge + labs(tag = 'B') + size
+# Panel 2
+p2 <- ggplot() +
+  geom_blank() +
+  lims(x = c(0, 1), y = c(0, 1)) +
+  coord_fixed() +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.grid.major.x = element_line(colour = 'grey', linetype = 'dotted')) +
+  size + 
+  labs(tag = 'B') +
+  annotation_custom(ggplotGrob(p_charge_profiles + size), xmin = 0, xmax = 1, ymin = 0.35, ymax = 0.68) +
+  annotate('text', x = 0.5, y = 0.975, label = str_wrap('Ligand binding is generally done by positive and negative subtype positions', 80)) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/cbs_asp_ionic.png'), interpolate = TRUE,
+                    xmin = 0.025, xmax = 0.225, ymin = 0.725, ymax = 0.925) +
+  annotate('text', x = 0.125, y = 0.7, label = str_wrap('Ionic interactions', 30)) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/tem1_asp_ligand.png'), interpolate = TRUE,
+                    xmin = 0.275, xmax = 0.475, ymin = 0.725, ymax = 0.925) +
+  annotate('text', x = 0.375, y = 0.7, label = str_wrap('Ion binding', 30)) + 
+  annotation_custom(ggplotGrob(p_charge_foldx + size), xmin = 0.025, xmax = 0.475, ymin = 0.1, ymax = 0.325)
 
-p3 <- p_tyr + labs(tag = 'C') + size
+# Panel 3
+p3 <- ggplot() +
+  geom_blank() +
+  lims(x = c(0, 1), y = c(0, 1)) +
+  coord_fixed() +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.grid.major.x = element_line(colour = 'grey', linetype = 'dotted')) +
+  size + 
+  labs(tag = 'C') +
+  annotation_custom(ggplotGrob(p_tyr_profiles + size), xmin = 0, xmax = 1, ymin = 0.38, ymax = 0.62) + 
+  annotation_custom(ggplotGrob(p_tyr_surface_acc + size), xmin = 0.025, xmax = 0.475, ymin = 0.75, ymax = 1) +
+  annotate('text', x = 0.25, y = 0.7, label = str_wrap('Permissive positions are surface accessible', 30)) +
+  annotation_custom(ggplotGrob(p_tyr_dssp + size), xmin = 0.525, xmax = 0.975, ymin = 0.75, ymax = 1) + 
+  annotation_raster(readPNG('figures/4_figures/position_examples/cbs_tyr_haem.png'), interpolate = TRUE,
+                    xmin = 0.025, xmax = 0.225, ymin = 0.15, ymax = 0.35) +
+  annotate('text', x = 0.125, y = 0.125, label = str_wrap('Y1 ligand binding', 30)) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/tp53_tyr_pi.png'), interpolate = TRUE,
+                    xmin = 0.275, xmax = 0.475, ymin = 0.15, ymax = 0.35) +
+  annotate('text', x = 0.375, y = 0.125, label = str_wrap('Pi orbital interactions', 30)) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/aph3ii_tyr_buried.png'), interpolate = TRUE,
+                    xmin = 0.525, xmax = 0.725, ymin = 0.15, ymax = 0.35) +
+  annotate('text', x = 0.625, y = 0.125, label = str_wrap('Y2 positions are buried', 30)) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/pab1_tyr_not_proline.png'), interpolate = TRUE,
+                    xmin = 0.775, xmax = 0.975, ymin = 0.15, ymax = 0.35) +
+  annotate('text', x = 0.875, y = 0.125, label = str_wrap('Y4 maintaining backbone orientation', 30))
+  
+# Panel 4
+p4 <- ggplot() +
+  geom_blank() +
+  lims(x = c(0, 1), y = c(0, 1)) +
+  coord_fixed() +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.grid.major.x = element_line(colour = 'grey', linetype = 'dotted')) +
+  size + 
+  labs(tag = 'D') +
+  annotate('text', x = 0.75, y = 0.25, label = str_wrap('Small/Large Hydrophobic Positions', 30))
 
-p4 <- p_hydro + labs(tag = 'D') + size
-
-figure4 <- multi_panel_figure(width = 200, height = 200, columns = 14, rows = 14,
+# Final Figure
+figure4 <- multi_panel_figure(width = 440, height = 400, columns = 11, rows = 10,
                               panel_label_type = 'none', row_spacing = 0.1, column_spacing = 0.1) %>%
-  # Legend
-  fill_panel(er_legend, row = 6:8, column = 13:14) %>%
-  
-  # Panel 1
-  fill_panel(p1_prof, row = 1, column = 1:3) %>%
-  fill_panel(p1_disulphide, row = 1, column = 4:5) %>%
-  fill_panel(p1_disulphide_ex, row = 1, column = 6) %>%
-  fill_panel(p1_sa, row = 2:3, column = 1:6) %>%
-  fill_panel(p1_zinc, row = 4:6, column = 1:2) %>%
-  fill_panel(p1_aromatic_1, row = 4:6, column = 3:4) %>%
-  fill_panel(p1_aromatic_2, row = 4:6, column = 5:6) %>%
-  
-  # Panel 2 
-  fill_panel(p2, row = 1:6, column = 7:12) %>%
-  
-  # Panel 3
-  fill_panel(p3, row = 7:12, column = 1:6) %>%
-  
-  # Panel 4  
-  fill_panel(p4, row = 7:12, column = 7:12)
+  fill_panel(er_legend, row = 5:6, column = 11) %>%
+  fill_panel(p1, row = 1:5, column = 1:5) %>%
+  fill_panel(p2, row = 1:5, column = 6:10) %>%
+  fill_panel(p3, row = 6:10, column = 1:5) %>%
+  fill_panel(p4, row = 6:10, column = 6:10)
 
 ggsave('figures/4_figures/figure4.pdf', figure4, width = figure_width(figure4), height = figure_height(figure4), units = 'mm')
 ggsave('figures/4_figures/figure4.png', figure4, width = figure_width(figure4), height = figure_height(figure4), units = 'mm')
