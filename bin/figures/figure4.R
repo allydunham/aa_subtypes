e4b25d4349095f866bd6ba1b7b089ca15e37da95
allydunham <ally@ebi.ac.uk>
2020-04-20 14:32:10 +0100

    Formatted figure 4 (cys examples in own figure)

--- bin/figures/figure4.R
+++ bin/figures/figure4.R
@@ -13,69 +13,91 @@ n_clusters <- nrow(full_characterisation$summary)
 
 er_limits <- c(min(full_characterisation$profiles$er), -min(full_characterisation$profiles$er))
 
-plot_profiles <- function(clusters, legend = FALSE){
-  filter(full_characterisation$profiles, cluster %in% clusters) %>%
-    mutate(mut = add_markdown(mut, AA_COLOURS),
-           cluster = add_markdown(cluster, cluster_number_colourmap(cluster))) %>%
-    ggplot(aes(x = mut, y = cluster, fill = er)) +
-    geom_raster(show.legend = legend) +
-    coord_fixed() +
-    scale_fill_distiller(type = ER_PROFILE_COLOURS$type, palette = ER_PROFILE_COLOURS$palette, direction = ER_PROFILE_COLOURS$direction, limits = er_limits) +
-    guides(fill = guide_colourbar(title = 'Normalised ER')) + 
-    theme(axis.text.x = element_markdown(),
-          axis.text.y = element_markdown(),
-          axis.title = element_blank(),
-          axis.ticks = element_blank(),
-          panel.grid.major.y = element_blank())
-}
+p_profiles <- filter(full_characterisation$profiles, cluster %in% c('C1', 'C2')) %>%
+  mutate(mut = add_markdown(mut, AA_COLOURS),
+         cluster = add_markdown(cluster, cluster_number_colourmap(cluster))) %>%
+  ggplot(aes(x = mut, y = cluster, fill = er)) +
+  geom_raster() +
+  coord_fixed() +
+  scale_fill_distiller(type = ER_PROFILE_COLOURS$type, palette = ER_PROFILE_COLOURS$palette, direction = ER_PROFILE_COLOURS$direction, limits = er_limits) +
+  guides(fill = guide_colourbar(title = 'Normalised ER', direction = 'horizontal')) + 
+  theme(axis.text.x = element_markdown(),
+        axis.text.y = element_markdown(),
+        axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid.major.y = element_blank(),
+        legend.position = 'bottom',
+        legend.title = element_text(vjust = 0.85))
 
-p_cys_profiles <- plot_profiles(c('C1', 'C2'))
-
-p_cys_surface_acc <- filter(dms, cluster %in% c('C1', 'C2')) %>%
+p_surface_acc <- filter(dms, cluster %in% c('C1', 'C2')) %>%
   ggplot(aes(x = all_atom_abs, y = ..scaled.., colour = cluster)) +
   geom_line(stat = 'density', show.legend = FALSE) +
   scale_colour_brewer(type = 'qual', palette = 'Set1') +
   labs(x = 'Surface Accessibility (All Atom Abs)', y = 'Scaled Density') +
   theme(legend.title = element_blank())
 
-p_cys_disulphide <- filter(dms, cluster %in% c('C1', 'C2'), !is.na(disulfide)) %>%
-  ggplot(aes(x = cluster, fill = cluster)) +
-  geom_bar(show.legend = FALSE) +
-  labs(y = 'Disulphide Bonds', x = '') +
-  coord_flip() +
+p_disulphide <- filter(dms, cluster %in% c('C1', 'C2'), !is.na(disulfide)) %>%
+  count(cluster) %>%
+  mutate(cluster = factor(cluster, levels = c('C1', 'C2')),
+         cluster_int = as.integer(cluster)) %>%
+  ggplot(aes(xmin = 0, xmax = n, ymin = as.integer(cluster) - 0.4, ymax = as.integer(cluster) + 0.4 , fill = cluster)) +
+  geom_rect(show.legend = FALSE) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/ccr5_cys_disulphide.png'), interpolate = TRUE, xmin=-27, xmax=-7, ymin=1, ymax=2) +
+  coord_fixed(ratio = 20, clip = 'off') +
+  scale_y_continuous(breaks = c(1, 2), labels = c("<span style='color:#E41A1C'>C1</span>", "<span style='color:#377EB8'>C2</span>")) +
+  scale_x_continuous(name = 'Disulphide Bonds', expand = expansion(0.01)) +
   scale_fill_brewer(type = 'qual', palette = 'Set1') +
-  theme(legend.title = element_blank(),
+  theme(axis.text.y = element_markdown(),
+        panel.grid.major.y = element_blank(),
+        plot.margin = unit(c(0.05, 0.05, 0.05, 0.22), 'npc'))
+
+p_ligand_ex <- ggplot() +
+  geom_blank() +
+  lims(x = c(0, 1.9), y = c(0, 0.9)) +
+  coord_fixed() +
+  annotation_raster(readPNG('figures/4_figures/position_examples/gal4_cys_zinc.png'), interpolate = TRUE, xmin=0, xmax=0.9, ymin=0, ymax=0.9) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/cbs_cys_haem.png'), interpolate = TRUE, xmin=1, xmax=1.9, ymin=0, ymax=0.9) +
+  labs(title = 'C1 Ligand Binding') +
+  theme(axis.text = element_blank(),
+        axis.title = element_blank(),
+        axis.ticks = element_blank(),
         panel.grid.major.y = element_blank())
+
+p_zinc_ex <- ggplot() +
+  geom_blank() +
+  coord_fixed() +
+  annotation_raster(readPNG('figures/4_figures/position_examples/gal4_cys_zinc.png'), interpolate = TRUE, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
+
+p_aromatic_ex1 <- ggplot() +
+  geom_blank() +
+  coord_fixed() +
+  annotation_raster(readPNG('figures/4_figures/position_examples/np_cys_aromatic.png'), interpolate = TRUE, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
+
+p_aromatic_ex2 <- ggplot() +
+  geom_blank() +
+  coord_fixed() +
+  annotation_raster(readPNG('figures/4_figures/position_examples/ccr5_cys_aromatic.png'), interpolate = TRUE, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
 
 ### Assemble figure ###
 size <- theme(text = element_text(size = 8))
-er_legend <- plot_profiles('A1', legend = TRUE) %>% get_legend() %>% as_ggplot() + size
 
-figure4 <- ggplot() +
-  geom_blank() +
-  lims(x = c(0, 1), y = c(0, 1)) +
-  coord_fixed() +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank(),
-        panel.grid.major.x = element_line(colour = 'grey', linetype = 'dotted')) +
-  size +
-  labs(tag = 'A') +
-  annotation_custom(ggplotGrob(p_cys_profiles + size), xmin = 0.15, xmax = 0.8, ymin = 0.4, ymax = 0.6) +
-  annotation_raster(readPNG('figures/4_figures/position_examples/ccr5_cys_disulphide.png'), interpolate = TRUE,
-                    xmin = 0.025, xmax = 0.2, ymin = 0.7, ymax = 0.875) +
-  annotation_custom(ggplotGrob(p_cys_disulphide + size), xmin = 0.2, xmax = 0.475, ymin = 0.7, ymax = 0.875) +
-  annotate('text', x = 0.25, y = 0.65, label = str_wrap('Most disulphide bonds are C1 positions', 25)) + 
-  annotation_custom(ggplotGrob(p_cys_surface_acc + size), xmin = 0.525, xmax = 0.975, ymin = 0.7, ymax = 0.975) +
-  annotate('text', x = 0.75, y = 0.65, label = str_wrap('C2 positions are more buried', 25)) +
-  annotation_raster(readPNG('figures/4_figures/position_examples/gal4_cys_zinc.png'), interpolate = TRUE,
-                    xmin = 0.1, xmax = 0.4, ymin = 0.1, ymax = 0.4) +
-  annotate('text', x = 0.25, y = 0.05, label = str_wrap('Other active roles are generally C1 positions', 25)) +
-  annotation_raster(readPNG('figures/4_figures/position_examples/ccr5_cys_aromatic.png'), interpolate = TRUE,
-                    xmin = 0.525, xmax = 0.73, ymin = 0.15, ymax = 0.355) +
-  annotation_raster(readPNG('figures/4_figures/position_examples/np_cys_aromatic.png'), interpolate = TRUE,
-                    xmin = 0.77, xmax = 0.975, ymin = 0.15, ymax = 0.355) +
-  annotate('text', x = 0.75, y = 0.05, label = str_wrap('Both C1 & C2 positions have aromatic interactions', 30))
+p1 <- p_profiles + size + labs(tag = 'A')
+p2_ex <- p_disulphide_ex + size + labs(tag = 'B')
+p2_bars <- p_disulphide + size + labs(tag = ' ')
+p3 <- p_surface_acc + size + labs(tag = 'C')
+p4 <- p_zinc_ex + size + labs(tag = 'D')
+p5_1 <- p_aromatic_ex1 + size + labs(tag = 'E')
+p5_2 <- p_aromatic_ex2 + size + labs(tag = ' ')
+ 
+figure4 <- multi_panel_figure(width = 200, height = 167, columns = 6, rows = 5,
+                              panel_label_type = 'none', row_spacing = 1, column_spacing = 1) %>%
+  fill_panel(p1, row = 1:2, column = 1:6) %>%
+  fill_panel(p2_ex, row = 3, column = 1) %>%
+  fill_panel(p2_bars, row = 3, column = 2:3) %>%
+  fill_panel(p3, row = 3, column = 4:6) %>%
+  fill_panel(p4, row = 4:5, column = 1:2) %>%
+  fill_panel(p5_1, row = 4:5, column = 3:4) %>%
+  fill_panel(p5_2, row = 4:5, column = 5:6)
 
-ggsave('figures/4_figures/figure4.pdf', figure4, width = 200, height = 200, units = 'mm')
-ggsave('figures/4_figures/figure4.png', figure4, width = 200, height = 200, units = 'mm')
+ggsave('figures/4_figures/figure4.pdf', figure4, figure_width(figure4), height = figure_height(figure4), units = 'mm')
+ggsave('figures/4_figures/figure4.png', figure4, figure_width(figure4), height = figure_height(figure4), units = 'mm')
