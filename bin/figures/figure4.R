c8a642f7812511565503f868248dc71b13cd318f
allydunham <ally@ebi.ac.uk>
2020-04-21 18:31:49 +0100

    Formatted figure 4 to column width (89mm)

--- bin/figures/figure4.R
+++ bin/figures/figure4.R
@@ -20,14 +20,18 @@ p_profiles <- filter(full_characterisation$profiles, cluster %in% c('C1', 'C2'))
   geom_tile(colour = 'grey') +
   coord_fixed() +
   scale_fill_distiller(type = ER_PROFILE_COLOURS$type, palette = ER_PROFILE_COLOURS$palette, direction = ER_PROFILE_COLOURS$direction, limits = er_limits) +
-  guides(fill = guide_colourbar(title = 'Normalised ER', direction = 'horizontal')) + 
+  guides(fill = guide_colourbar(title = 'Normalised ER', direction = 'horizontal', barheight = unit(2, 'mm'))) + 
   theme(axis.text.x = element_markdown(),
         axis.text.y = element_markdown(),
         axis.title = element_blank(),
         axis.ticks = element_blank(),
         panel.grid.major.y = element_blank(),
+        panel.spacing = unit(1, 'mm'),
         legend.position = 'bottom',
-        legend.title = element_text(vjust = 0.85))
+        legend.title = element_text(vjust = 0.85),
+        legend.margin = margin(0, 0, 0, 0, 'mm'),
+        legend.box.margin = margin(-3, 0, 0, 0, 'mm'),
+        legend.background = element_blank())
 
 p_surface_acc <- filter(dms, cluster %in% c('C1', 'C2')) %>%
   ggplot(aes(x = all_atom_abs, y = ..scaled.., colour = cluster)) +
@@ -42,14 +46,19 @@ p_disulphide <- filter(dms, cluster %in% c('C1', 'C2'), !is.na(disulfide)) %>%
          cluster_int = as.integer(cluster)) %>%
   ggplot(aes(xmin = 0, xmax = n, ymin = as.integer(cluster) - 0.4, ymax = as.integer(cluster) + 0.4 , fill = cluster)) +
   geom_rect(show.legend = FALSE) +
-  annotation_raster(readPNG('figures/4_figures/position_examples/ccr5_cys_disulphide.png'), interpolate = TRUE, xmin=-27, xmax=-7, ymin=1, ymax=2) +
+  annotation_raster(readPNG('figures/4_figures/position_examples/ccr5_cys_disulphide.png'), interpolate = TRUE, xmin=-35, xmax=-15, ymin=1, ymax=2) +
   coord_fixed(ratio = 20, clip = 'off') +
-  scale_y_continuous(breaks = c(1, 2), labels = c("<span style='color:#E41A1C'>C1</span>", "<span style='color:#377EB8'>C2</span>")) +
-  scale_x_continuous(name = 'Disulphide Bonds', expand = expansion(0.01)) +
+  # Fake y axis to position image beyond it
+  annotate('richtext', x = -7.5, y = c(1, 2), fill = NA, label.color = NA, size = 2,
+           label = c("<span style='color:#E41A1C'>**C1**</span>", "<span style='color:#377EB8'>**C2**</span>")) +
+  scale_x_continuous(name = 'Disulphide Bonds', expand = expansion(0.01), limits = c(-40, 80), breaks = seq(0, 80, 20)) +
   scale_fill_brewer(type = 'qual', palette = 'Set1') +
-  theme(axis.text.y = element_markdown(),
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.title.x = element_text(hjust = 0.7),
         panel.grid.major.y = element_blank(),
-        plot.margin = unit(c(0.05, 0.05, 0.05, 0.22), 'npc'))
+        plot.margin = unit(c(0.01, 0.01, 0.01, 0.01), 'npc'))
 
 p_ligand_ex <- ggplot() +
   geom_blank() +
@@ -61,7 +70,8 @@ p_ligand_ex <- ggplot() +
   theme(axis.text = element_blank(),
         axis.title = element_blank(),
         axis.ticks = element_blank(),
-        panel.grid.major.y = element_blank())
+        panel.grid.major.y = element_blank(),
+        plot.margin = unit(c(0, 0, 0, 0), 'mm'))
 
 p_aromatic_ex <- ggplot() +
   geom_blank() +
@@ -73,19 +83,20 @@ p_aromatic_ex <- ggplot() +
   theme(axis.text = element_blank(),
         axis.title = element_blank(),
         axis.ticks = element_blank(),
-        panel.grid.major.y = element_blank())
+        panel.grid.major.y = element_blank(),
+        plot.margin = unit(c(0, 0, 0, 0), 'mm'))
 
 ### Assemble figure ###
-size <- theme(text = element_text(size = 8))
+size <- theme(text = element_text(size = 5))
 
-p1 <- p_profiles + size
-p2 <- p_disulphide + size
-p3 <- p_surface_acc + size
-p4 <- p_ligand_ex + size
-p5 <- p_aromatic_ex + size
+p1 <- p_profiles + labs(tag = 'A') + size
+p2 <- p_disulphide + labs(tag = 'B') + size
+p3 <- p_surface_acc + labs(tag = 'C') + size
+p4 <- p_ligand_ex + labs(tag = 'D') + size
+p5 <- p_aromatic_ex + labs(tag = 'E') + size
  
-figure4 <- multi_panel_figure(width = 200, height = 167, columns = 2, rows = 3,
-                              panel_label_type = 'upper-alpha', row_spacing = 5, column_spacing = 5) %>%
+figure4 <- multi_panel_figure(width = 89, height = 74.315, columns = 2, rows = 3,
+                              panel_label_type = 'none', row_spacing = 0, column_spacing = 0) %>%
   fill_panel(p1, row = 1, column = 1:2) %>%
   fill_panel(p2, row = 2, column = 1) %>%
   fill_panel(p3, row = 2, column = 2) %>%
