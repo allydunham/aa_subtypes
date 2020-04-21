14386492b3c58db123fcc2a2a2c241764c42bd9f
allydunham <ally@ebi.ac.uk>
2020-04-21 14:06:27 +0100

    Reformatted to correct size (183mm, A4 page width)

--- bin/figures/figure1.R
+++ bin/figures/figure1.R
@@ -22,11 +22,11 @@ gene_summary <- group_by(dms, gene) %>%
             n_struct = n_distinct(position[!is.na(total_energy)])) %>%
   mutate(percent_struct = n_struct / n * 100,
          x = str_c(n, " (", signif(percent_struct, 3), "%)"),
-         img = str_c("<img src='figures/4_figures/proteins/", gene_to_filename(gene), ".png", "' width='70' />"))
+         img = str_c("<img src='figures/4_figures/proteins/", gene_to_filename(gene), ".png", "' width='55' />"))
 
 p_genes <- ggplot(gene_summary, aes(x = x, y = n, label = img)) +
   geom_richtext(fill = NA, label.color = NA, label.padding = grid::unit(rep(0, 4), "pt")) +
-  facet_wrap(~gene, nrow = 4, scales = 'free') +
+  facet_wrap(~gene, nrow = 5, scales = 'free') +
   theme(panel.grid.major.y = element_blank(),
         axis.text.y = element_blank(),
         axis.ticks = element_blank(),
@@ -51,7 +51,7 @@ p_norm_trans <- filter(raw, study %in% c('steinberg_2016_tem1', 'heredia_2018_cc
   labs(title = 'Transformed') + 
   guides(colour = FALSE) +
   coord_cartesian(clip = 'off') +
-  scale_x_continuous(breaks = c(0, -5), labels = c("<span style='font-size:6pt'><span style='line-height:4'>0</span><br>Neutral</span>", "<img src='figures/4_figures/parts/arrow.png' width='40' /><span style='font-size:6pt'><br>Deleterious</span>")) +
+  scale_x_continuous(breaks = c(0, -6.5), labels = c("<span style='font-size:6pt'><span style='line-height:4'>0</span><br>Neutral</span>", "<img src='figures/4_figures/parts/arrow.png' width='40' /><span style='font-size:6pt'><br>Deleterious</span>")) +
   scale_y_continuous(expand = expansion(0,0)) +
   theme(text = element_text(size = 8),
         axis.text.y = element_blank(),
@@ -81,11 +81,11 @@ p_norm <- ggplot() +
   coord_cartesian(clip = 'off') +
   labs(title = 'Combining Deep Mutational\nScanning Studies') + 
   annotate('polygon', x = c(0.25, 0.5, 0.35, 0.5, 0, 0.15, 0), y = c(0.25, 0.5, 0.5, 2, 2, 0.5, 0.5), fill = 'lightskyblue') +
-  annotate('text', label = 'Process double\nmutants and\nmultiple mutations', x = 0.25, y = 1.85) +
-  annotate('text', label = 'Transform scores\nto standard scale', x = 0.25, y = 1.45) +
-  annotate('text', label = 'Normalise against\nmean of lowest\n10% of scores', x = 0.25, y = 1) +
-  annotate('text', label = 'Filter positions\nwith scores for\n<16 substitutions', x = 0.25, y = 0.5) +
-  annotate('richtext', label = '**Comparable<br>ER Scores**', x = 0.25, y = 0.15, fill = NA, label.colour = NA) +
+  annotate('richtext', label = "<span style='font-size:8pt'>Process double<br>mutants and<br>multiple mutations</span>", x = 0.25, y = 1.85, fill = NA, label.colour = NA) +
+  annotate('richtext', label = "<span style='font-size:8pt'>Transform scores<br>to standard scale</span>", x = 0.25, y = 1.45, fill = NA, label.colour = NA) +
+  annotate('richtext', label = "<span style='font-size:8pt'>Normalise against<br>mean of lowest<br>10% of scores</span>", x = 0.25, y = 1, fill = NA, label.colour = NA) +
+  annotate('richtext', label = "<span style='font-size:8pt'>Filter positions<br>with scores for<br>&lt;16 substitutions</span>", x = 0.25, y = 0.5, fill = NA, label.colour = NA) +
+  annotate('richtext', label = "<span style='font-size:10pt'>**Comparable<br>ER Scores**</span>", x = 0.25, y = 0.15, fill = NA, label.colour = NA) +
   annotation_custom(ggplotGrob(p_norm_raw), xmin = 0.55, xmax = 1.1, ymin = 1.5, ymax = 2.1) +
   annotation_custom(ggplotGrob(p_norm_trans), xmin = 0.55, xmax = 1.1, ymin = 0.8, ymax = 1.5) + 
   annotation_custom(ggplotGrob(p_norm_final), xmin = 0.55, xmax = 1.1, ymin = 0, ymax = 0.7) +
@@ -106,10 +106,12 @@ p_rep_ubi <- ggplot(ubi_studys, aes(x=roscoe_2013_ubi, y=roscoe_2014_ubi, colour
   geom_smooth(method = 'lm', formula = y ~ x, colour = 'black') +
   geom_abline(slope = 1, colour = 'black', linetype = 'dotted') +
   scale_colour_manual(values = MUT_CLASS_COLOURS) +
+  scale_x_continuous(expand = expansion(0.01)) +
+  scale_y_continuous(expand = expansion(0.01)) +
   guides(colour = FALSE) +
-  coord_equal() +
   labs(x = 'Roscoe et al. 2013', y = 'Roscoe & Bolon 2014') +
-  theme(panel.grid.major.y = element_blank())
+  theme(panel.grid.major.y = element_blank(),
+        axis.ticks = element_blank())
 
 ### Panel 4 - Blosum Correlation ###
 blosum_cor <- group_by(raw, wt, mut) %>%
@@ -121,8 +123,12 @@ p_blosum <- ggplot(blosum_cor, aes(x = blosum62, y = score, colour = mut_class))
   geom_jitter(width = 0.2, shape = 20) +
   geom_smooth(data = filter(blosum_cor, mut_class == 'Missense'), mapping = aes(x = blosum62, y = score), colour = 'black', method = 'lm', formula = y ~ x, inherit.aes = FALSE) +
   scale_colour_manual(values = MUT_CLASS_COLOURS) +
-  guides(colour = guide_legend(title = '', override.aes = list(size = 2))) +
-  labs(x = 'BLOSUM62', y = 'Mean Normalised ER')
+  guides(colour = guide_legend(title = '', override.aes = list(size = 2), direction = 'horizontal')) +
+  labs(x = 'BLOSUM62', y = 'Mean Normalised ER') +
+  theme(legend.position = 'bottom')
+
+p_variant_legend <- get_legend(p_blosum) %>% as_ggplot()
+p_blosum <- p_blosum + guides(colour = FALSE)
 
 ### Panel 5 Sift correlation ###
 format_gene <- function(gene, study){
@@ -146,12 +152,13 @@ p_sift <- ggplot(sift_correlations, aes(x = gene_pretty, y = estimate, fill = p_
   scale_y_continuous(expand = expansion(mult = c(0.01, 0.05))) +
   labs(x='', y=expression("Pearson's"~rho)) +
   scale_fill_viridis_d(drop=FALSE) +
+  guides(fill = guide_legend(nrow = 2)) +
   theme(axis.ticks.x = element_blank(),
-        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 7),
         legend.title = element_blank(),
         legend.text = element_markdown(size = 6),
         legend.key.size = unit(2, 'mm'),
-        legend.position = 'right')
+        legend.position = 'bottom')
 
 ### Figure Assembly ###
 size <- theme(text = element_text(size = 8))
@@ -159,14 +166,16 @@ p1 <- p_genes + labs(tag = 'A') + size
 p2 <- p_norm + labs(tag = 'B') + size
 p3 <- p_rep_ubi + labs(tag = 'C') + size
 p4 <- p_blosum + labs(tag = 'D') + size
+p34_legend <- p_variant_legend + size
 p5 <- p_sift + labs(tag = 'E') + size
 
-figure1 <- multi_panel_figure(width = 300, height = 200, columns = 4, rows = 3,
-                              panel_label_type = 'none', row_spacing = 0.1, column_spacing = 0.1) %>%
-  fill_panel(p1, row = 1:2, column = 1:3) %>%
-  fill_panel(p2, row = 1:2, column = 4) %>%
-  fill_panel(p3, row = 3, column = 1) %>%
-  fill_panel(p4, row = 3, column = 2) %>%
-  fill_panel(p5, row = 3, column = 3:4)
+figure1 <- multi_panel_figure(width = 183, height = 220, columns = 20, rows = 20,
+                              panel_label_type = 'none', row_spacing = 0, column_spacing = 0) %>%
+  fill_panel(p1, row = 1:14, column = 1:14) %>%
+  fill_panel(p2, row = 1:14, column = 15:20) %>%
+  fill_panel(p3, row = 15:19, column = 1:5) %>%
+  fill_panel(p4, row = 15:19, column = 6:10) %>%
+  fill_panel(p34_legend, row = 20, column = 1:10) %>%
+  fill_panel(p5, row = 15:20, column = 11:20)
 ggsave('figures/4_figures/figure1.pdf', figure1, width = figure_width(figure1), height = figure_height(figure1), units = 'mm')
 ggsave('figures/4_figures/figure1.png', figure1, width = figure_width(figure1), height = figure_height(figure1), units = 'mm')
