5c52ce95b3b410df8a1c772b4e98ae239d5aca65
allydunham <ally@ebi.ac.uk>
2020-05-20 16:39:42 +0100

    Added supplementary figure showing distribution of mean cosine distances between profiles in ER and sift clustering

---
+++ bin/figures/figureS9.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env Rscript
+# Figure S9 (SIFT subtypes)
+source('src/config.R')
+
+dms <- left_join(read_tsv('data/subtypes/final_subtypes.tsv'),
+                  read_tsv('data/subtypes/sift_scores.tsv') %>% rename(sift_cluster = cluster),
+                  by = c('study', 'gene', 'position', 'wt')) %>%
+  left_join(read_tsv('data/combined_mutational_scans.tsv'), by = c('study', 'gene', 'position', 'wt'))
+
+dms_dist <- filter(dms, !str_detect(cluster, CLUSTER_OUTLIER_RE)) %>%
+  group_by(wt, cluster) %>%
+  summarise_at(vars(A:Y), mean) %>%
+  group_map(~tibble_to_matrix(., A:Y) %>% 
+              cosine_distance_matrix() %>% 
+              rowMeans() %>% 
+              set_names(.x$cluster)) %>%
+  unlist()
+
+sift_dist <- filter(dms, !str_detect(sift_cluster, CLUSTER_OUTLIER_RE)) %>%
+  group_by(wt, sift_cluster) %>%
+  summarise_at(vars(starts_with('log10_sift')), mean) %>%
+  group_map(~tibble_to_matrix(., starts_with('log10_sift')) %>% 
+              cosine_distance_matrix() %>% 
+              rowMeans() %>% 
+              set_names(.x$sift_cluster)) %>%
+  unlist()
+
+cosine <- tibble(type=c(rep('dms', length(dms_dist)), rep('sift', length(sift_dist))),
+                 cluster=c(names(dms_dist), names(sift_dist)),
+                 cosine=c(dms_dist, sift_dist) %>% unname())
+
+figure <- ggplot(cosine, aes(x = type, y = cosine, fill = type)) +
+  geom_boxplot(show.legend = FALSE) +
+  stat_compare_means(paired = FALSE, comparisons = list(c('dms', 'sift'))) +
+  scale_x_discrete(name = 'Profiles used for clustering', labels = c(dms='ER', sift='log<sub>10</sub>SIFT')) +
+  scale_y_continuous(name = 'Mean Cosine Distance', limits = c(0, 0.6)) +
+  scale_fill_manual(values = c(dms='firebrick2', sift='cornflowerblue')) +
+  theme(axis.text.x = element_markdown(),
+        axis.ticks.x = element_blank())
+
+ggsave('figures/4_figures/figureS9.pdf', figure, width = 120, height = 120, units = 'mm')
+ggsave('figures/4_figures/figureS9.png', figure, width = 120, height = 120, units = 'mm')
