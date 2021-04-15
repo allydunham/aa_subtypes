a73956f2aef85ff0bda49ee8a8c356b0901dc93b
allydunham <ally@ebi.ac.uk>
2021-04-15 11:06:24 +0100

    Enhanced normalisation checks

--- bin/analysis/0_data/check_normalisation.R
+++ bin/analysis/0_data/check_normalisation.R
@@ -12,22 +12,31 @@ quantiles <- filter(dms, !is.na(score)) %>%
             without = quantile(transformed_score[!mut == "*"], 0.1, na.rm = TRUE),
             nonsense = any(mut == "*"))
 
-plots$norm_quantiles <- ggplot(quantiles[quantiles$nonsense], aes(x = with, y = without)) +
+plots$norm_quantiles <- ggplot(filter(quantiles, nonsense), aes(x = with, y = without)) +
   geom_point() +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
   labs(x = "With Nonsense", y = "Without Nonsense")
 
 ### Check distribution of bottom 10% scores predictor metrics ###
-dms_scores <- select(dms, study, transformed_score, log10_sift, total_energy) %>%
+dms_scores <- select(dms, study, score, transformed_score, log10_sift, total_energy) %>%
   left_join(quantiles, by = "study") %>%
   mutate(with10 = transformed_score < with,
-         without10 = transformed_score < without)
+         without10 = transformed_score < without,
+         neutral = abs(score) < 0.3)
 
-bot10 <- filter(dms_scores, nonsense, with10 | without10) %>%
+score_groups <- filter(dms_scores, nonsense, with10 | without10 | neutral) %>%
   pivot_longer(c(log10_sift, total_energy), names_to = "metric", values_to = "value") %>%
-  pivot_longer(ends_with("10"), names_to = "class", values_to = "member") %>%
+  pivot_longer(c(with10, without10, neutral), names_to = "class", values_to = "member") %>%
   filter(member, !is.na(value))
 
-plots$bottom_10_scores <- ggplot(bot10, aes(x = value, y = ..scaled.., colour = class)) +
+plots$score_group_density <- ggplot(score_groups, aes(x = value, y = ..scaled.., colour = class)) +
   stat_density(geom = "line", position = "identity") +
   facet_wrap(~metric, nrow = 1, scales = "free_x")
+
+plots$score_group_box <- ggplot(score_groups, aes(x = class, y = value, fill = class)) +
+  geom_boxplot() +
+  facet_wrap(~metric, nrow = 1, scales = "free_x") +
+  stat_compare_means(comparisons = list(c("with10", "without10"), c("with10", "neutral"), c("without10", "neutral")))
+
+### Save plots ###
+save_plotlist(plots, "figures/0_data/")
